08c5a1689ab15b48034be69ef51d4327ddbb01c1
Faizan Ghani <faizanghani2222@gmail.com>
2022-06-18 10:10:28 +0530

    footer layout updated

--- footer layout/footer.js
+++ footer layout/footer.js
@@ -1,11 +1,11 @@
-let dropdown=document.querySelectorAll(".ddown");
+let ddown=document.querySelectorAll(".ddown");
 let hflex=document.querySelectorAll(".hflex");
 let drop=document.querySelectorAll(".drop");
 
-for(let i=0;i<dropdown.length;i++){
-    dropdown[i].addEventListener("click",()=>{
+for(let i=0;i<ddown.length;i++){
+    ddown[i].addEventListener("click",()=>{
         hflex[i].classList.toggle('active');
-        dropdown[i].classList.toggle('active');
+        ddown[i].classList.toggle('active');
         if( drop[i].innerText=="-"){
             drop[i].innerText="+"
         }else{
